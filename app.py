e941a8e012a1006e5efe0a6d456a4c6fbcc26382
Phunda <phunda73@gmail.com>
2023-03-30 17:53:00 +0300

    5 laba

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
-
 
 app = Flask(__name__)
 
@@ -17,18 +16,36 @@ def index():
     return render_template('login.html')
 
 
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
+    return render_template('login.html')
+
+
 cursor = conn.cursor()
 
 
-@app.route('/login/', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if not username or not password:
-        return render_template('login.html', Error="Пустой логин или пароль")
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
-    if len(records) > 0:
-        return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
-    else:
-        return render_template('login.html', Error="Пользователя нет в базе данных либо неверный логин или пароль")
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        if not name or not login or not password:
+            return render_template('new.html', Error="Не все поля заполнены")
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+        return redirect('/login/')
+    return render_template('registration.html')
+
